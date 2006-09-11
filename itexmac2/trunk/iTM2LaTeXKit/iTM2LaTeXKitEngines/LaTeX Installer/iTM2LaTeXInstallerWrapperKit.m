bf23ece27ea3d0aae979f57dd10a6a3e833e3a80
jlaurens <jlaurens@e30227a4-e615-0410-b69e-ef4e86b9efcb>
2006-09-11 23:37:41 +0000

    Unstable: addint LaTeX Engines

---
+++ itexmac2/trunk/iTM2LaTeXKit/iTM2LaTeXKitEngines/LaTeX Installer/iTM2LaTeXInstallerWrapperKit.m
@@ -0,0 +1,83 @@
+/*
+//
+//  @version Subversion: $Id: iTM2LaTeXInstallerWrapperKit.m 49 2006-06-23 13:12:37Z jlaurens $ 
+//
+//  Created by jlaurens AT users DOT sourceforge DOT net on Tue Jan 18 10:34:50 GMT 2005.
+//  Copyright © 2005 Laurens'Tribune. All rights reserved.
+//
+//  This program is free software; you can redistribute it and/or modify it under the terms
+//  of the GNU General Public License as published by the Free Software Foundation; either
+//  version 2 of the License, or any later version, modified by the addendum below.
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details. You should have received a copy
+//  of the GNU General Public License along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  GPL addendum: Any simple modification of the present code which purpose is to remove bug,
+//  improve efficiency in both code execution and code reading or writing should be addressed
+//  to the actual developper team.
+//
+//  Version history: (format "- date:contribution(contributor)") 
+//  To Do List: (format "- proposition(percentage actually done)")
+*/
+
+#import "iTM2LaTeXInstallerWrapperKit.h"
+
+NSString * const iTM2LaTeXInstaller_use_output = @"iTM2_pstopdf_use_output";// "0" (default) or "1"(bool)
+NSString * const iTM2LaTeXInstaller_output = @"iTM2_pstopdf_output";//-o ""
+NSString * const iTM2LaTeXInstaller_write_to_log = @"iTM2_pstopdf_write_to_log";//-l
+NSString * const iTM2LaTeXInstaller_progress_message = @"iTM2_pstopdf_progress_message";// -p
+
+@implementation iTM2EngineLaTeXInstaller
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  engineMode
++ (NSString *)engineMode;
+/*"Description forthcoming.
+Version History: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Thu Nov 18 07:53:25 GMT 2004
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    return @"iTM2_Engine_LaTeXInstaller";
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  inputFileExtensions
++ (NSArray *)inputFileExtensions;
+/*"Description forthcoming.
+Version History: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Thu Nov 18 07:53:25 GMT 2004
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    return [NSArray arrayWithObjects:@"ins", nil];
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  defaultShellEnvironment
++ (NSDictionary *)defaultShellEnvironment;
+/*"Description forthcoming.
+Version History: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Thu Nov 18 07:53:25 GMT 2004
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    return [NSDictionary dictionary];
+}
+@end
+
+@implementation iTM2MainInstaller(iTM2LaTeXInstaller)
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  iTM2LaTeXInstallerCompleteInstallation
++ (void)iTM2LaTeXInstallerCompleteInstallation;
+/*"Description forthcoming.
+Version History: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Thu Nov 18 07:53:25 GMT 2004
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    [iTM2EngineLaTeXInstaller installBinary];
+//iTM2_END;
+    return;
+}
+@end
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= iTM2LaTeXInstallerWrapperKit
