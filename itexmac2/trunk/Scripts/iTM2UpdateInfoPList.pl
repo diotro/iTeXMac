eddddb47c8d94a167b9b0739f973fc0dc8b65b53
jlaurens <jlaurens@e30227a4-e615-0410-b69e-ef4e86b9efcb>
2006-09-25 23:11:03 +0000

    new algorithm to find the revision based on the project .mode1 files

--- itexmac2/trunk/Scripts/iTM2UpdateInfoPList.pl
+++ itexmac2/trunk/Scripts/iTM2UpdateInfoPList.pl
@@ -1,33 +1,40 @@
 #!/usr/bin/env perl
 # iTM2UpdateInfoPList.pl
-# © Laurens'Tribune, Dimanche 24 Septembre 2006
+# © Laurens'Tribune, Mardi 26 Septembre 2006
 printf "warning: Updating the build number\n";
 printf "Getting the svn revision number...";
 $TARGET_BUILD_DIR = "$ENV{TARGET_BUILD_DIR}";
-$REVISION=`/usr/local/bin/svn info "$TARGET_BUILD_DIR/../History.txt"`;
-if( $REVISION =~ m/.*Revision: (\d*).*/s )
+$REVISION=0;
+@CANDIDATES=split('\0', `find "$TARGET_BUILD_DIR/.." -regex ".*\.mode1" -print0`);
+while(my $FILE = shift(@CANDIDATES))
 {
-	$REVISION="$1";
-	printf "REVISION: $REVISION";
-	$FULL_INFOPLIST_PATH="$TARGET_BUILD_DIR/$ENV{INFOPLIST_PATH}";
-	printf "\nUpdating $FULL_INFOPLIST_PATH";
-	open INPUT, "< $FULL_INFOPLIST_PATH" or die "Can't open $FULL_INFOPLIST_PATH : $!";
-	$content = do { local $/; <INPUT> };
-	close INPUT;
-	if( $content =~ s/__\(REVISION\)__/$REVISION/g )
+	printf "\n$FILE";
+	$revision=`/usr/local/bin/svn info "$FILE"`;
+	if( $revision =~ m/.*Revision: (\d*).*/s )
 	{
-		open OUTPUT, "> $FULL_INFOPLIST_PATH" or die "Can't open $FULL_INFOPLIST_PATH : $!";
-		print OUTPUT "$content";
-		close OUTPUT;
-		printf "\nwarning: Updating the build number... DONE\n";
+		$revision="$1";
+		if($revision > $REVISION)
+		{
+			$REVISION = $revision;
+		}
 	}
-	else
-	{
-		printf "\nwarning: Updating the build number... FAILED\n";
-	}
+}
+$FULL_INFOPLIST_PATH="$TARGET_BUILD_DIR/$ENV{INFOPLIST_PATH}";
+printf "\nUpdating $FULL_INFOPLIST_PATH";
+open INPUT, "< $FULL_INFOPLIST_PATH" or die "Can't open $FULL_INFOPLIST_PATH : $!";
+$content = do { local $/; <INPUT> };
+close INPUT;
+if( $content =~ s/__\(REVISION\)__/$REVISION/g )
+{
+	open OUTPUT, "> $FULL_INFOPLIST_PATH" or die "Can't open $FULL_INFOPLIST_PATH : $!";
+	print OUTPUT "$content";
+	close OUTPUT;
+	printf "\nwarning: Updating the build number to revision $REVISION... DONE\n";
 }
 else
 {
-	printf "\nwarning: Updating the build number... SKIPPED\n";
+	printf "\nwarning: Updating the build number... FAILED\n";
 }
 exit 0;
+
+#
