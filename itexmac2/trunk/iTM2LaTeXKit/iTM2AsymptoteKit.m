2d1dd34caafb275204e33e6377dad7c2ac4c4f93
jlaurens <jlaurens@e30227a4-e615-0410-b69e-ef4e86b9efcb>
2006-11-13 09:22:46 +0000

    Asymptote source skeletton added

---
+++ itexmac2/trunk/iTM2LaTeXKit/iTM2AsymptoteKit.m
@@ -0,0 +1,374 @@
+/*
+//  iTM2AsymptoteKit.h
+//  iTeXMac2
+//
+//  @version Subversion: $Id: iTM2AsymptoteKit.m 251 2006-10-23 19:51:58Z jlaurens $ 
+//
+//  Created by jlaurens AT users DOT sourceforge DOT net on Sun Jun 24 2001.
+//  Copyright © 2001-2004 Laurens'Tribune. All rights reserved.
+//
+//  This program is free software; you can redistribute it and/or modify it under the terms
+//  of the GNU General Public License as published by the Free Software Foundation; either
+//  version 2 of the License, or any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details. You should have received a copy
+//  of the GNU General Public License along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  GPL addendum: Any simple modification of the present code which purpose is to remove bug,
+//  improve efficiency in both code execution and code reading or writing should be addressed
+//  to the actual developper team.
+*/
+
+#import "iTM2AsymptoteKit.h"
+
+NSString * const iTM2AsymptoteInspectorMode = @"Asymptote Mode";
+NSString * const iTM2AsymptoteToolbarIdentifier = @"iTM2 Asymptote Toolbar: Tiger";
+
+@implementation iTM2AsymptoteInspector
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  inspectorMode
++ (NSString *)inspectorMode;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Fri Sep 05 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+//iTM2_END;
+    return iTM2AsymptoteInspectorMode;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= windowPositionShouldBeObserved
+- (BOOL)windowPositionShouldBeObserved;
+/*"Subclasses will return YES.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Thu Jul 21 16:05:20 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    return YES;
+}
+@end
+
+NSString * const iTM2ToolbarAsymptoteLabelItemIdentifier = @"AsymptoteLabel";
+NSString * const iTM2ToolbarAsymptoteSectionItemIdentifier = @"AsymptoteSection";
+//NSString * const iTM2ToolbarDoZoomToFitItemIdentifier = @"doZoomToFit";
+
+@implementation iTM2MainInstaller(AsymptoteInspectorToolbar)
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  AsymptoteInspectorToolbarCompleteInstallation
++ (void)AsymptoteInspectorToolbarCompleteInstallation;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+	[SUD registerDefaults: [NSDictionary dictionaryWithObjectsAndKeys:
+		[NSNumber numberWithBool:NO], @"iTM2AsymptoteToolbarAutosavesConfiguration",
+		[NSNumber numberWithBool:YES], @"iTM2AsymptoteToolbarShareConfiguration",
+			nil]];
+//iTM2_END;
+	return;
+}
+@end
+
+#define DEFINE_TOOLBAR_ITEM(SELECTOR)\
++ (NSToolbarItem *)SELECTOR;{return [self toolbarItemWithIdentifier:[self identifierFromSelector:_cmd] inBundle:[iTM2TeXInspector classBundle]];}
+
+@implementation NSToolbarItem(iTM2Asymptote)
+DEFINE_TOOLBAR_ITEM(AsymptoteSectionToolbarItem)
+DEFINE_TOOLBAR_ITEM(AsymptoteLabelToolbarItem)
+@end
+
+NSString * const iTM2ToolbarAsymptoteSubscriptItemIdentifier = @"subscript";
+NSString * const iTM2ToolbarAsymptoteSuperscriptItemIdentifier = @"superscript";
+
+@implementation iTM2AsymptoteInspector(Toolbar)
+#pragma mark =-=-=-=-=-  TOOLBAR
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  setupToolbarWindowDidLoad
+- (void)setupToolbarWindowDidLoad;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    NSToolbar * toolbar = [[[NSToolbar alloc] initWithIdentifier:iTM2AsymptoteToolbarIdentifier] autorelease];
+	NSString * key = [NSString stringWithFormat:@"NSToolbar Configuration %@", [toolbar identifier]];
+	if([self contextBoolForKey:@"iTM2AsymptoteToolbarShareConfiguration" domain:iTM2ContextAllDomainsMask])
+	{
+		NSDictionary * configDictionary = [SUD dictionaryForKey:key];
+		if([configDictionary count])
+		{
+			[toolbar setConfigurationFromDictionary:configDictionary];
+			if(![[toolbar items] count])
+			{
+				[SUD removeObjectForKey:key];
+				toolbar = [[[NSToolbar alloc] initWithIdentifier:iTM2AsymptoteToolbarIdentifier] autorelease];
+			}
+		}
+	}
+	else
+	{
+		NSDictionary * configDictionary = [SUD dictionaryForKey:key];
+//iTM2_LOG(@"configDictionary: %@", configDictionary);
+		configDictionary = [self contextDictionaryForKey:key domain:iTM2ContextAllDomainsMask];
+//iTM2_LOG(@"configDictionary: %@", configDictionary);
+		if([configDictionary count])
+			[toolbar setConfigurationFromDictionary:configDictionary];
+		if(![[toolbar items] count])
+		{
+			configDictionary = [SUD dictionaryForKey:key];
+//iTM2_LOG(@"configDictionary: %@", configDictionary);
+			[self takeContextValue:nil forKey:key domain:iTM2ContextAllDomainsMask];
+			if([configDictionary count])
+				[toolbar setConfigurationFromDictionary:configDictionary];
+			if(![[toolbar items] count])
+			{
+				[SUD removeObjectForKey:key];
+				toolbar = [[[NSToolbar alloc] initWithIdentifier:iTM2AsymptoteToolbarIdentifier] autorelease];
+			}
+		}
+	}
+	[toolbar setAutosavesConfiguration:YES];
+    [toolbar setAllowsUserCustomization:YES];
+//    [toolbar setSizeMode:NSToolbarSizeModeSmall];
+    [toolbar setDelegate:self];
+    [[self window] setToolbar:toolbar];
+//iTM2_END;
+	return;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  toggleShareToolbarConfiguration:
+- (void)toggleShareToolbarConfiguration:(id)sender;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+	BOOL old = [self contextBoolForKey:@"iTM2AsymptoteToolbarShareConfiguration" domain:iTM2ContextAllDomainsMask];
+	[self takeContextBool: !old forKey:@"iTM2AsymptoteToolbarShareConfiguration" domain:iTM2ContextAllDomainsMask];
+	[self validateWindowContent];
+//iTM2_END;
+	return;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  validateToggleShareToolbarConfiguration:
+- (BOOL)validateToggleShareToolbarConfiguration:(id)sender;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+	[sender setState: ([self contextBoolForKey:@"iTM2AsymptoteToolbarShareConfiguration" domain:iTM2ContextAllDomainsMask]? NSOnState:NSOffState)];
+//iTM2_END;
+	return YES;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  prepareToolbarConfigurationCompleteSaveContext:
+- (void)prepareToolbarConfigurationCompleteSaveContext:(id)sender;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+	if([self contextBoolForKey:@"iTM2AsymptoteToolbarAutosavesConfiguration" domain:iTM2ContextAllDomainsMask])
+	{
+		NSToolbar * toolbar = [[self window] toolbar];
+		NSString * key = [NSString stringWithFormat:@"NSToolbar Configuration %@", [toolbar identifier]];
+		[self takeContextValue:[toolbar configurationDictionary] forKey:key domain:iTM2ContextAllDomainsMask];
+	}
+//iTM2_START;
+	return;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  toolbarDefaultItemIdentifiers:
+- (NSArray *)toolbarDefaultItemIdentifiers:(NSToolbar *)toolbar;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    // Required delegate method:  Returns the ordered list of items to be shown in the toolbar by default
+    // If during the toolbar's initialization, no overriding values are found in the usermodel, or if the
+    // user chooses to revert to the default items this set will be used
+//iTM2_END;
+    return [NSArray arrayWithObjects:
+				NSToolbarPrintItemIdentifier,
+				NSToolbarSeparatorItemIdentifier,
+				iTM2ToolbarTypesetItemIdentifier,
+				iTM2ToolbarProjectTerminalItemIdentifier,
+				NSToolbarSeparatorItemIdentifier,
+				iTM2ToolbarProjectSettingsItemIdentifier,
+				iTM2ToolbarProjectFilesItemIdentifier,
+				NSToolbarSeparatorItemIdentifier,
+				iTM2ToolbarAsymptoteSubscriptItemIdentifier,
+				iTM2ToolbarAsymptoteSuperscriptItemIdentifier,
+//				iTM2ToolbarBookmarkItemIdentifier,
+//				iTM2ToolbarAsymptoteLabelItemIdentifier,
+//				iTM2ToolbarAsymptoteSectionItemIdentifier,
+						nil];
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  toolbarAllowedItemIdentifiers:
+- (NSArray *)toolbarAllowedItemIdentifiers:(NSToolbar *)toolbar;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Tue May  3 16:20:26 GMT 2005
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    // Required delegate method:  Returns the list of all allowed items by identifier.  By default, the toolbar
+    // does not assume any items are allowed, even the separator.  So, every allowed item must be explicitly listed
+    // The set of allowed items is used to construct the customization palette
+//iTM2_END;
+    return [NSArray arrayWithObjects:
+					iTM2ToolbarTypesetItemIdentifier,
+					iTM2ToolbarProjectTerminalItemIdentifier,
+					iTM2ToolbarProjectSettingsItemIdentifier,
+					iTM2ToolbarProjectFilesItemIdentifier,
+//					iTM2ToolbarBookmarkItemIdentifier,
+					NSToolbarSeparatorItemIdentifier,
+					NSToolbarPrintItemIdentifier,
+					NSToolbarSpaceItemIdentifier,
+					NSToolbarFlexibleSpaceItemIdentifier,
+					NSToolbarCustomizeToolbarItemIdentifier,
+//					NSToolbarShowGraphicssItemIdentifier,
+//					NSToolbarShowFontsItemIdentifier,
+							nil];
+}
+@end
+
+#define BUNDLE [self classBundle]
+#define TABLE @"iTM2InsertAsymptote"
+
+@implementation iTM2AsymptoteEditor
+@end
+
+@implementation iTM2AsymptoteWindow
+@end
+
+@implementation iTM2AsymptoteParserAttributesServer
+@end
+
+static id _iTM2AsymptoteModeForModeArray = nil;
+
+#define _TextStorage (iTM2TextStorage *)_TS
+
+@implementation iTM2AsymptoteParser
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= load
++ (void)load;
+/*"Description forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- < 1.1: 03/10/2002
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+	iTM2_INIT_POOL;
+//iTM2_START;
+//iTM2_LOG(@"iTM2TeXParser");
+    if(!_iTM2AsymptoteModeForModeArray)
+        _iTM2AsymptoteModeForModeArray = [[NSArray arrayWithObjects:@"include", @"includegraphics", @"url", nil] retain];
+	iTM2_RELEASE_POOL;
+//iTM2_END;
+    return;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= syntaxParserStyle
++ (NSString *)syntaxParserStyle;
+/*"Designated initializer.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2: 12/05/2003
+To Do List: Nothing
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+//iTM2_END;
+    return @"Asymptote";
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  defaultModesAttributes
++ (NSDictionary *)defaultModesAttributes;
+/*"Description forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Wed Dec 17 09:32:38 GMT 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+//iTM2_END;
+    return [super defaultModesAttributes];
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  syntaxModeForCharacter:previousMode:
+- (unsigned)syntaxModeForCharacter:(unichar)theChar previousMode:(unsigned)previousMode;
+/*"Description forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Fri Dec 12 22:44:56 GMT 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+//iTM2_END;
+	return [super syntaxModeForCharacter:theChar previousMode:previousMode];
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  syntaxModeForLocation:previousMode:effectiveLength:nextModeIn:before:
+- (unsigned)syntaxModeForLocation:(unsigned)location previousMode:(unsigned)previousMode effectiveLength:(unsigned *)lengthRef nextModeIn:(unsigned *)nextModeRef before:(unsigned)beforeIndex;
+/*"Description forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Fri Dec 12 22:44:56 GMT 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+//iTM2_END;
+	return [super syntaxModeForLocation:location previousMode:previousMode effectiveLength:lengthRef nextModeIn:nil before:beforeIndex];
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  attributesAtIndex:effectiveRange:
+- (NSDictionary *)attributesAtIndex:(unsigned)aLocation effectiveRange:(NSRangePointer)aRangePtr;
+/*"Description forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 1.4: Wed Dec 17 09:32:38 GMT 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+//iTM2_END;
+	return [super attributesAtIndex:aLocation effectiveRange:aRangePtr];
+}
+@end
+
+NSString * const iTM2AsymptoteParserAttributesInspectorType = @"iTM2AsymptoteParserAttributes";
+
+@implementation iTM2AsymptoteParserAttributesDocument
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  inspectorType
++ (NSString *)inspectorType;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Fri Sep 05 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    return iTM2AsymptoteParserAttributesInspectorType;
+}
+@end
+
+@implementation iTM2AsymptoteParserAttributesInspector
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  inspectorType
++ (NSString *)inspectorType;
+/*"Description Forthcoming.
+Version history: jlaurens AT users DOT sourceforge DOT net
+- 2.0: Fri Sep 05 2003
+To Do List:
+"*/
+{iTM2_DIAGNOSTIC;
+//iTM2_START;
+    return iTM2AsymptoteParserAttributesInspectorType;
+}
+@end
+
