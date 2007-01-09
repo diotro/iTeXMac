37356ab5b4bfd0ac9f3de91af3ad8f1c028b688d
jlaurens <jlaurens@e30227a4-e615-0410-b69e-ef4e86b9efcb>
2007-01-09 17:37:58 +0000

    New build number method

---
+++ itexmac2/trunk/Scripts/PrepareBuildNumber.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+# iTM2UpdateInfoPList.pl
+# © Laurens'Tribune, Mardi 26 Septembre 2006
+printf "warning: Updating the build number\n";
+printf "Getting the svn revision number...";
+$TARGET_BUILD_DIR = "$ENV{TARGET_BUILD_DIR}";
+$REVISION=0;
+@CANDIDATES=split('\0', `find "$TARGET_BUILD_DIR/.." -regex ".*\.mode1" -print0`);
+while(my $FILE = shift(@CANDIDATES))
+{
+	printf "\n$FILE";
+	$revision=`/usr/local/bin/svn info "$FILE"`;
+	if( $revision =~ m/.*Revision: (\d*).*/s )
+	{
+		$revision="$1";
+		if($revision > $REVISION)
+		{
+			$REVISION = $revision;
+		}
+	}
+}
+$INFOPLIST_PREFIX_HEADER="$TARGET_BUILD_DIR/INFOPLIST_PREFIX_HEADER";
+printf "\nUpdating $INFOPLIST_PREFIX_HEADER";
+open OUTPUT, "> $INFOPLIST_PREFIX_HEADER" or die "Can't open $INFOPLIST_PREFIX_HEADER : $!";
+print OUTPUT "#define SVN_BUILD_NUMBER $REVISION\n";
+$DATE=`date -u`;
+print OUTPUT "#define SVN_BUILD_DATE $DATE\n";
+close OUTPUT;
+printf "\nwarning: Updating the build number to revision $REVISION... DONE\n";
+exit 0;
+
+#
