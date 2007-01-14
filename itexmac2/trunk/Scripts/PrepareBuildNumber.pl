38ff93fe0e587f3233d45b35ba2f86407d99525a
jlaurens <jlaurens@e30227a4-e615-0410-b69e-ef4e86b9efcb>
2007-01-14 16:14:45 +0000

    Sanity

--- itexmac2/trunk/Scripts/PrepareBuildNumber.pl
+++ itexmac2/trunk/Scripts/PrepareBuildNumber.pl
@@ -2,22 +2,30 @@
 # iTM2UpdateInfoPList.pl
 # © Laurens'Tribune, Mardi 26 Septembre 2006
 printf "warning: Updating the build number\n";
-printf "Getting the svn revision number...";
+$CONFIGURATION = "$ENV{CONFIGURATION}";
 $TARGET_BUILD_DIR = "$ENV{TARGET_BUILD_DIR}";
 $REVISION=0;
-@CANDIDATES=split('\0', `find "$TARGET_BUILD_DIR/.." -regex ".*\.mode1" -print0`);
-while(my $FILE = shift(@CANDIDATES))
+if($CONFIGURATION =~ m/Deployment/)
 {
-	printf "\n$FILE";
-	$revision=`/usr/local/bin/svn info "$FILE"`;
-	if( $revision =~ m/.*Revision: (\d*).*/s )
+printf "Getting the svn revision number ($CONFIGURATION is Deployment)...";
+	@CANDIDATES=split('\0', `find "$TARGET_BUILD_DIR/.." -regex ".*\.mode1" -print0`);
+	while(my $FILE = shift(@CANDIDATES))
 	{
-		$revision="$1";
-		if($revision > $REVISION)
+		printf "\n$FILE";
+		$revision=`/usr/local/bin/svn info "$FILE"`;
+		if( $revision =~ m/.*Revision: (\d*).*/s )
 		{
-			$REVISION = $revision;
+			$revision="$1";
+			if($revision > $REVISION)
+			{
+				$REVISION = $revision;
+			}
 		}
 	}
+}
+else
+{
+	$REVISION = "NONE";
 }
 $INFOPLIST_PREFIX_HEADER="$TARGET_BUILD_DIR/INFOPLIST_PREFIX_HEADER";
 printf "\nUpdating $INFOPLIST_PREFIX_HEADER";
@@ -28,3 +36,5 @@ print OUTPUT "#define SVN_BUILD_DATE $DATE\n";
 close OUTPUT;
 printf "\nwarning: Updating the build number to revision $REVISION... DONE\n";
 exit 0;
+
+#
