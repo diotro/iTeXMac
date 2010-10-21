ab82e99dd996c7201ad37fcbd0924ebb3ed09a26
jlaurens <jlaurens@e30227a4-e615-0410-b69e-ef4e86b9efcb>
2010-10-21 23:06:13 +0000

    Sparkle Synchronization Script Fixed

--- itexmac2/branches/814+/Scripts/iTM2SynchronizeSparkle.sh
+++ itexmac2/branches/814+/Scripts/iTM2SynchronizeSparkle.sh
@@ -8,25 +8,27 @@ DESTINATION="build/${PRODUCT_NAME}.framework"
 echo DESTINATION is ${DESTINATION}
 if ! [ -L "${DESTINATION}/${PRODUCT_NAME}" ]
 then
-	CANDIDATE="$(find . -regex "./${PRODUCT_NAME}/${PRODUCT_NAME}/.*Garbage.*/${PRODUCT_NAME}\.framework" -print)"
+	CANDIDATE="$(find . -regex "./${PRODUCT_NAME}/${PRODUCT_NAME}/.*Garbage.*/${PRODUCT_NAME}\.framework/${PRODUCT_NAME}" -print)"
 	echo CANDIDATE is "${CANDIDATE}"
-	if ! [ -L "${CANDIDATE}/${PRODUCT_NAME}" ]
+	if ! [ -L "${CANDIDATE}" ]
 	then
 		echo expanding the zip
 		pushd "$PRODUCT_NAME"
-		unzip "$PRODUCT_NAME.zip"
+		unzip -oq "$PRODUCT_NAME.zip" -d "$PRODUCT_NAME"
+        find "$PRODUCT_NAME" -name ".svn" -exec rm -Rf "{}" \;
 		popd
-		CANDIDATE="$(find . -regex "./${PRODUCT_NAME}/${PRODUCT_NAME}/.*Garbage.*/${PRODUCT_NAME}\.framework" -print)"
+		CANDIDATE="$(find . -regex "./${PRODUCT_NAME}/${PRODUCT_NAME}/.*Garbage.*/${PRODUCT_NAME}\.framework/${PRODUCT_NAME}" -print)"
 	fi
+    CANDIDATE="$(dirname "${CANDIDATE}")"
 	echo CANDIDATE is "${CANDIDATE}"
 	echo "warning: iTeXMac2 INFO, Importing ${PRODUCT_NAME}.framework"
 	if [ -e "${DESTINATION}" ]
 	then
 	   rm -Rf "${DESTINATION}"
     else
-        mkdir -p "`dirname "${DESTINATION}"`"
+        mkdir -p "$(dirname "${DESTINATION}")"
     fi
-	cp -Rf "${CANDIDATE}" "${DESTINATION}"
+	cp -R "${CANDIDATE}" "$(dirname ${DESTINATION})"
 	echo "warning: iTeXMac2 INFO, Importing frameworks from ${PRODUCT_NAME}... DONE"
 	exit 0
 fi
